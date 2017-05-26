a18e239cad5d1c66022085fe2b18b23749352b0f
Rusherz <ieatrusherz34@gmail.com>
2017-05-26 19:07:31 -0400

    Starting auth project

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var path = require('path');
+var cookieSession = require('cookie-session');
+var request = require('request');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
